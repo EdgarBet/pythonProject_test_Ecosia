0ad452193e2619c8e93be2fd2260d3f9683f55d8
Eduard <133205683+EdgarBet@users.noreply.github.com>
2023-09-01 14:04:44 +0300

    Add files via upload
    Update

--- tests/first_test.py
+++ tests/first_test.py
@@ -3,31 +3,43 @@
 # from selene import browser, have
 # from selene.support.shared import config
 # from selene.support.shared.jquery_style import s, ss
-
-# config.window_width = 1920
-# config.window_height = 1080
 #
-# browser.open('https://itstep.org/uk')
-# time.sleep(5)
+# def test_itstep_count():
+#     config.window_width = 1920
+#     config.window_height = 1080
 #
-# ss('.marker-city.cremenchug-small .marker-point').first.click()
-# page_text = browser.driver.page_source.lower()
-# count = page_text.count('it step')
-# print('Вираз it step повторюється:', count, 'рази')
+#     browser.open('https://itstep.org/uk')
+#     time.sleep(5)
+#
+#     ss('.marker-city.cremenchug-small .marker-point').first.click()
+#     page_text = browser.driver.page_source.lower()
+#     count = page_text.count('it step')
+#     print('Вираз it step повторюється:', count, 'рази')
 
 import time
 from selene.api import browser, s, ss
 
-def test_search_and_count():
-    browser.open('https://duckduckgo.com/')
-    s('[name="q"]').type('шахтар').press_enter()
-    time.sleep(5)
-    ss(".result__url").element(2).click()
+# def test_search_and_count():
+#     browser.open('https://duckduckgo.com/')
+#     s('[name="q"]').type('шахтар').press_enter()
+#     time.sleep(5)
+#     ss(".result__url").element(2).click()
+#
+#     page_text = browser.driver.page_source.lower()
+#     word_to_count = "шахтар"
+#     count = page_text.count(word_to_count)
+#     print(f'Слово "{word_to_count}" зустрічається на сторінці {count} раз(ів)')
+#     assert count > 0, f'Слово "{word_to_count}" не знайдено на сторінці'
+#     browser.quit()
 
-    page_text = browser.driver.page_source.lower()
-    word_to_count = "шахтар"
-    count = page_text.count(word_to_count)
-    print(f'Слово "{word_to_count}" зустрічається на сторінці {count} раз(ів)')
-    assert count > 0, f'Слово "{word_to_count}" не знайдено на сторінці'
-    browser.quit()
+def test_duckduckgo_search():
+   browser.open('https://duckduckgo.com/')
+   browser.element('[name="q"]').type('шахтар').press_enter()
 
+   time.sleep(5)  # wait for the page to load
+
+   results = [elem.get_attribute('innerHTML') for elem in browser.all('.result__a')]
+
+   count = sum(1 for result in results if 'шахтар' in result)
+
+   assert count > 0
